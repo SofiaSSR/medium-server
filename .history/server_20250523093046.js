e4349ea77302bddbff6d04d099f7d302dd89f666
Sofia <unitargetofsofi@gmail.com>
2025-05-23 09:33:59 +0200

    implemented server

---
+++ .history/server_20250523093046.js
@@ -0,0 +1,26 @@
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+  if (req.url === '/api/data' && req.method === 'GET') {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');)
+
+    const responseData = {
+      message: 'Here is your f data',
+      timestamp: new Date().toISOString(),
+    };
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(responseData));
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ .history/server_20250523093129.js
@@ -0,0 +1,26 @@
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer(async (req, res) => {
+  if (req.url === '/api/data' && req.method === 'GET') {
+    const response = await fetch('http://ec2-35-90-236-177.us-west-2.compute.amazonaws.com:3000/transactions/filter/pending');
+
+    const responseData = {
+      message: 'Here is your f data',
+      timestamp: new Date().toISOString(),
+    };
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(responseData));
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

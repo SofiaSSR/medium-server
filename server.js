71588889e91141c94a8f96de58a3c0525c640a8a
Sofia <unitargetofsofi@gmail.com>
2025-05-29 15:10:51 +0200

    update transactions and users usage

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const axios = require("axios");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const baseUrl = `http://ec2-35-90-236-177.us-west-2.compute.amazonaws.com:${PORT}`;
 // Habilitar CORS para cualquier origen
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
@@ -19,17 +19,34 @@ app.use(express.json());
 
 // Ruta para obtener transacciones desde el servicio externo
 
-app.use("/api", async (req, res) => {
+app.use("/api/transactions", async (req, res) => {
   try {
     const targetPath = req.header("X-Target-Path");
 
     if (!targetPath || typeof targetPath !== "string") {
       return res.status(400).json({ message: "Missing Target Path " });
     }
+    const targetUrl = `${baseUrl}${targetPath}`;
 
-    const targetUrl = `http://ec2-35-90-236-177.us-west-2.compute.amazonaws.com:3000${targetPath}`;
-    console.log("Proxying request to:", targetUrl);
-
+    console.log("Proxying transactions request to:", targetUrl);
+    switch (req.header("method")) {
+      case "POST":
+        const response = axios.post(targetUrl, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          params: req.params,
+          ...req.body,
+        });
+        res.status(response.status).send(response.data);
+        return;
+      case "PUT":
+        return res.status(405).json({ message: "Method Not Allowed" });
+      case "DELETE":
+        return res.status(405).json({ message: "Method Not Allowed" });
+      case "PATCH":
+        return res.status(405).json({ message: "Method Not Allowed" });
+    }
     if (
       targetPath.includes("date-range") ||
       targetPath.includes("filter-mixed")
@@ -64,7 +81,7 @@ app.use("/api", async (req, res) => {
       });
       res.status(response.status).send(response.data);
       return;
-    };
+    }
 
     const response = await axios.get(targetUrl, {
       headers: {
@@ -83,27 +100,82 @@ app.use("/api", async (req, res) => {
   }
 });
 
-  function getDateRange(period) {
-    const end_date = new Date();
-    let start_date;
-    if (period === "fortnight")
-      start_date = new Date(
-        end_date.getFullYear(),
-        end_date.getMonth(),
-        end_date.getDate() - 15
-      );
-    if (period === "month")
-      start_date = new Date(
-        end_date.getFullYear(),
-        end_date.getMonth() - 1,
-        end_date.getDate()
-      );
-    if (period === "all") start_date = new Date(0);
+function getDateRange(period) {
+  const end_date = new Date();
+  let start_date;
+  if (period === "fortnight")
+    start_date = new Date(
+      end_date.getFullYear(),
+      end_date.getMonth(),
+      end_date.getDate() - 15
+    );
+  if (period === "month")
+    start_date = new Date(
+      end_date.getFullYear(),
+      end_date.getMonth() - 1,
+      end_date.getDate()
+    );
+  if (period === "all") start_date = new Date(0);
   return {
     end_date,
     start_date,
   };
 }
+
+
+app.use("/api/users", async (req, res) => {
+  try {
+    const targetPath = req.header("X-Target-Path");
+
+    if (!targetPath || typeof targetPath !== "string") {
+      return res.status(400).json({ message: "Missing Target Path " });
+    }
+
+    const targetUrl = `${baseUrl}${targetPath}`;
+    console.log("Proxying users request to:", targetUrl);
+    // Verificar el método HTTP
+    switch (req.header("method")) {
+      case "POST":
+        const response = await axios.post(targetUrl, {
+          ...req.params,
+          ...req.body,
+        });
+
+        res.status(response.status).send(response.data);
+        return;
+      case "PUT":
+        return res.status(405).json({ message: "Method Not Allowed" });
+      case "DELETE":
+        return res.status(405).json({ message: "Method Not Allowed" });
+      case "PATCH":
+        return res.status(405).json({ message: "Method Not Allowed" });
+    }
+
+    const response = await axios.get(
+      targetUrl,
+      {
+        params: req.params,
+        ...req.body,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error("Proxy error:", error.message);
+    res.status(error.status).json({
+      message: "Proxy POST failed",
+      error: error.message,
+      error: error.response ? error.response.data : "No response data",
+    });
+  }
+});
+
 
 // Ruta de health check
 app.get("/health", (req, res) => {
